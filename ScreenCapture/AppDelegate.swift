c24b76bd7c4abfec58e198ed3c652a97a2b4430b
Vuong, Thanh T <vuong@mmi-1.cs.helsinki.fi>
2016-10-26 12:16:26 +0300

    Initial Commit

---
+++ ScreenCapture/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ScreenCapture
+//
+//  Created by Vuong, Thanh T on 26/10/16.
+//  Copyright © 2016 Vuong, Thanh T. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
